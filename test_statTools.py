509f32cb7b19f4e6c449fb1c40bf17e0b6ed0d51
NicholasGin <nicholasgin@gmail.com>
2018-11-08 20:11:38 -0500

    passes test case 3 with negative numbers

--- test_statTools.py
+++ test_statTools.py
@@ -5,4 +5,7 @@ def test_mean_basic1():
     assert(mean([1,2,3,4,5]) == 3)
 
 def test_mean_basic2():
-    assert(mean([93, 91, 90, 96, 89, 93 ]) == 92)
+    assert(mean([93, 91, 90, 96, 89, 93]) == 92)
+
+def test_mean_negatives():
+    assert(mean([-5, -8,-23,87, -30,56]) == 77/6)
