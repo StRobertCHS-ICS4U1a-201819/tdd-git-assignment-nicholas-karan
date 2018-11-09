3396f7b5f34d461e4c4571092a65ef958eeb50a0
NicholasGin <nicholasgin@gmail.com>
2018-11-08 19:23:33 -0500

    passes basic test case 1

--- statTools.py
+++ statTools.py
@@ -1,1 +1,6 @@
 
+#mean, mode, lower quartile, variance
+#retun error if non-unique mode
+
+def mean(intList):
+    return 3
--- test_statTools.py
+++ test_statTools.py
@@ -1,1 +1,6 @@
+import pytest
+from statTools import *
+
+def test_mean_basic1():
+    assert(mean([1,2,3,4,5]) == 3)
 
