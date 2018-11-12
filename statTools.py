288ac07f2232cc00c5c5b47a3194f85f9ad5e107
Karan <polobotu@gmail.com>
2018-11-11 23:25:20 -0500

    got it to count how many times the mode number occurs

--- statTools.py
+++ statTools.py
@@ -1,3 +1,9 @@
-def mode(num1, num2, num3):
-    #stage 1: program has failed with no code
-
+def mode(numList):
+    counter_list = []
+    for i in range(len(numList)):
+        # loop based on the length of our data array
+        counter_list.append(numList.count(numList[i]))
+        # list of arrays counting frequency of values
+    maxFreq = max(counter_list)
+    return maxFreq
+    # found maximum frequency of value, so value appears 3 times
--- test_statTools.py
+++ test_statTools.py
@@ -2,4 +2,10 @@ import statTools
 from statTools import *
 
 def test_mode():
-    assert(mode(4, 5, 10) == false)
+    assert(mode([4, 5, 10]) == -1)
+
+def test_mode2():
+    assert(mode([]) == -1)
+
+def test_mode3():
+    assert(mode([1, 1, 2, 3, 4, 5, 5, 5, 5, 5]) == 5)
