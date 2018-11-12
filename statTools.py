ab44d8d51de3bbe2ef0d61320af193f853653fdb
Karan <polobotu@gmail.com>
2018-11-11 20:04:54 -0500

    ran first test, program didnt work, no code

--- statTools.py
+++ statTools.py
@@ -1,1 +1,3 @@
+def mode(num1, num2, num3):
+    #stage 1: program has failed with no code
 
--- test_statTools.py
+++ test_statTools.py
@@ -1,1 +1,5 @@
+import statTools
+from statTools import *
 
+def test_mode():
+    assert(mode(4, 5, 10) == false)
