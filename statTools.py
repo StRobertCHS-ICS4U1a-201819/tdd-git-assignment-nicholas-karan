d721db8709b1cdabca05767f1c7830133c343bd5
Karan <polobotu@gmail.com>
2018-11-14 21:40:57 -0500

    This should be the working code

--- statTools.py
+++ statTools.py
@@ -1,9 +1,26 @@
 def mode(numList):
-    counter_list = []
-    for i in range(len(numList)):
-        # loop based on the length of our data array
-        counter_list.append(numList.count(numList[i]))
-        # list of arrays counting frequency of values
-    maxFreq = max(counter_list)
-    return maxFreq
-    # found maximum frequency of value, so value appears 3 times
+    try:
+        counter_list = []
+        for i in range(len(numList)):
+            # loop based on the length of our data array
+            counter_list.append(numList.count(numList[i]))
+            # list of arrays counting frequency of values
+        maxFreq = max(counter_list)
+        if maxFreq == 1:
+            # if there are no modes, and the counter counts the mode once, its a failed test
+            return "no Mode"
+        # found maximum frequency of value, so value appears in list a certain number times
+        else:
+            maxList = []
+            for x in numList:
+                if (numList.count(x) == maxFreq):
+                    maxList.append(x)
+                    # extract that value
+            mode = []
+            for x in maxList:
+                if (x not in mode):
+                # remove duplicates
+                    mode.append(x)
+            return mode
+    except:
+        return -1
--- test_statTools.py
+++ test_statTools.py
@@ -1,11 +1,12 @@
-import statTools
+import pytest
 from statTools import *
 
 def test_mode():
-    assert(mode([4, 5, 10]) == -1)
+    assert(mode([4, 5, 10]) == "no Mode")
 
 def test_mode2():
     assert(mode([]) == -1)
 
 def test_mode3():
-    assert(mode([1, 1, 2, 3, 4, 5, 5, 5, 5, 5]) == 5)
+    assert(mode([1, 1, 2, 3, 4, 5, 5, 5, 5, 5]) == [5])
+
