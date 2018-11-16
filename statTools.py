fc6086d26328d8130e6f9429a07de357b9d664ec
NicholasGin <nicholasgin@gmail.com>
2018-11-15 22:37:32 -0500

    add comments, fix whitespace and variable names

--- statTools.py
+++ statTools.py
@@ -1,43 +1,53 @@
+"""
+-------------------------------------------------------------------------------
+Name:		statTools.py
 
-#mean, median, lower quartile, upper quartile
+Purpose:    Contains functions for Measures of Central Tendency and Measures of Spread
+            Includes: mean, median, lower quartile, upper quartile
 
-#refactor this later
-def mean(intList):
-    if len(intList) == 0:
+Author:		Gin. N
+
+Created:    08/11/2018
+------------------------------------------------------------------------------
+"""
+
+
+def mean(int_list):
+    if len(int_list) == 0:
         raise ValueError("An empty list was provided")
     try:
         total = 0
-        for i in intList:
+        for i in int_list:
             total += i
 
-        return total / len(intList)
+        return total / len(int_list)
     except TypeError:
         raise TypeError("List contains a non integer value")
 
 
-def median(intList):
+def median(int_list):
     try:
-        intList.sort()
+        int_list.sort()
 
-        if len(intList) == 0:
+        if len(int_list) == 0:
             raise ValueError("An empty list was provided")
-        elif len(intList) % 2 == 0:
-            return (intList[(len(intList) // 2) - 1] + intList[len(intList) // 2]) / 2
+        elif len(int_list) % 2 == 0:
+            return (int_list[(len(int_list) // 2) - 1] + int_list[len(int_list) // 2]) / 2
         else:
-            return intList[len(intList) // 2]
+            return int_list[len(int_list) // 2]
     except TypeError:
         raise TypeError("List contains a non integer value")
 
 
-def lQ(intList):
+def lower_quartile(int_list):
     try:
-        if len(intList) < 4:
+        if len(int_list) < 4:
             return -1
-        intList.sort()
-        lower_half = intList[:len(intList) // 2]
+        int_list.sort()
+        lower_half = int_list[:len(int_list) // 2]
 
 
-        if len(intList) % 2 == 0:
+        if len(int_list) % 2 == 0:
             value1 = lower_half[len(lower_half) // 2 - 1]
             value2 = lower_half[len(lower_half) // 2]
             return(value1 + value2) / 2
@@ -46,14 +56,15 @@ def lQ(intList):
     except TypeError:
         raise TypeError("List contains a non integer value")
 
-def uQ(intList):
+
+def upper_quartile(int_list):
     try:
-        if len(intList) < 4:
+        if len(int_list) < 4:
             return -1
 
-        intList.sort()
-        upper_half = intList[len(intList) // 2:]
-        if len(intList) % 2 == 0:
+        int_list.sort()
+        upper_half = int_list[len(int_list) // 2:]
+        if len(int_list) % 2 == 0:
             value1 = upper_half[len(upper_half) // 2 - 1]
             value2 = upper_half[len(upper_half) // 2]
             return(value1 + value2) / 2
--- test_statTools.py
+++ test_statTools.py
@@ -1,98 +1,132 @@
+"""
+-------------------------------------------------------------------------------
+Name:		test_statTools.py
+
+Purpose:    Contains test cases for fucntions of Measures of Central Tendency and Measures of Spread
+
+Author:		Gin. N
+
+Created:    08/11/2018
+------------------------------------------------------------------------------
+"""
+
 import pytest
 from statTools import *
 
 # mean tests
+
+
 def test_mean_basic1():
     assert(mean([1, 2, 3, 4, 5]) == 3)
+
 
 def test_mean_basic2():
     assert(mean([93, 91, 90, 96, 89, 93]) == 92)
 
+
 def test_mean_negative():
     assert(mean([-5, -8, -23, -87, 30, 56]) == -37/6)
 
+
 def test_mean_negative2():
     assert(mean([-9, -8, -5, -3, -1]) == -5.2)
+
 
 def test_mean_empty_List():
     with pytest.raises(ValueError) as error:
         mean([])
     assert("An empty list was provided" == str(error.value))
 
+
 def test_mean_illegal_input():
     with pytest.raises(TypeError) as error:
         mean([1, 2, 3, 'a'])
     assert("List contains a non integer value" == str(error.value))
 
+# median tests
 
-
-#median tests
 
 def test_median_basic1():
     assert(median([1, 2, 3, 4, 5, 6, 7]) == 4)
 
+
 def test_median_basic2():
     assert(median([5, 6, 7, 8, 9, 10]) == 7.5)
+
 
 def test_median_basic3():
     assert(median([22, 77, 73, 59, 95, 31, 66, 14]) == 62.5)
 
+
 def test_median_negatives():
     assert(median([-9, -8, -5, -3, -1]) == -5)
+
 
 def test_median_empty_list():
     with pytest.raises(ValueError) as error:
         median([])
     assert("An empty list was provided" == str(error.value))
 
+
 def test_median_illegal_input():
     with pytest.raises(TypeError) as error:
         median([1, 2, 3, 'a'])
     assert("List contains a non integer value" == str(error.value))
 
-#lower quartile tests
+# lower quartile tests
 
-def test_lQ_basic1():
-    assert(lQ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 2.5)
 
-def test_lQ_basic2():
-    assert(lQ([12, 23, 34, 45, 46, 57, 58, 59, 67, 78, 100]) == 34)
+def test_lower_quartile_basic1():
+    assert(lower_quartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 2.5)
 
-def test_lq_basic3():
-    assert(lQ([7, 9, 3, 4, 9, 11, 18, 6]) == 5)
 
-def test_lq_empty_List():
-    assert(lQ([]) == -1)
+def test_lower_quartile_basic2():
+    assert(lower_quartile([12, 23, 34, 45, 46, 57, 58, 59, 67, 78, 100]) == 34)
 
-def test_lq_less_than4():
-    assert(lQ([1,2,3]) == -1)
 
-def test_lq_illegal_input():
+def test_lower_quartile_basic3():
+    assert(lower_quartile([7, 9, 3, 4, 9, 11, 18, 6]) == 5)
+
+
+def test_lower_quartile_empty_list():
+    assert(lower_quartile([]) == -1)
+
+
+def test_lower_quartile_less_than4():
+    assert(lower_quartile([1, 2, 3]) == -1)
+
+
+def test_lower_quartile_illegal_input():
     with pytest.raises(TypeError) as error:
-        lQ([1, 2, 3, 'a'])
+        lower_quartile([1, 2, 3, 'a'])
     assert("List contains a non integer value" == str(error.value))
 
-#def test_lq_illegal_imput():
 
-#upper quartile tests
+# upper quartile tests
 
-def test_uq_basic1():
-    assert (uQ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 7.5)
+def test_upper_quartile_basic1():
+    assert (upper_quartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 7.5)
 
-def test_uq_basic2():
-    assert(uQ([1, 1, 3, 4, 6, 7, 7, 9, 10, 11, 14, 17]) == 10.5)
 
-def test_uq_basic3():
-    assert(uQ([7, 9, 3, 4, 9, 11, 18, 6]) == 10)
 
-def test_uq_empty_list():
-    assert(uQ([]) == -1)
+def test_upper_quartile_basic2():
+    assert(upper_quartile([1, 1, 3, 4, 6, 7, 7, 9, 10, 11, 14, 17]) == 10.5)
 
-def test_uq_less_than4():
-    assert(uQ([1,2,3]) == -1)
 
-def test_uq_illegal_input():
+def test_upper_quartile_basic3():
+    assert(upper_quartile([7, 9, 3, 4, 9, 11, 18, 6]) == 10)
+
+
+def test_upper_quartile_empty_list():
+    assert(upper_quartile([]) == -1)
+
+
+def test_upper_quartile_less_than4():
+    assert(upper_quartile([1, 2, 3]) == -1)
+
+
+def test_upper_quartile_illegal_input():
     with pytest.raises(TypeError) as error:
-        uQ([1, 2, 3, 'a'])
+        upper_quartile([1, 2, 3, 'a'])
     assert("List contains a non integer value" == str(error.value))
 
