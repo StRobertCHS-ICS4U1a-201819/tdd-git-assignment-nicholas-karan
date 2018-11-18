d62b7bd9d1d03405662f16e7b0c25ffa4a35e16e
Karan <polobotu@gmail.com>
2018-11-17 21:19:49 -0500

    reduced number of failed tests from 13 to 2, clearing them up

--- statTools.py
+++ statTools.py
@@ -1,71 +1,83 @@
 import math
 def mode(numList):
-    try:
-        if len(numList) == 1:
-            raise ValueError("no mode if there is only one value")
+    for i in range (len(numList)):
+        if isinstance(numList[i], str) == True:
+            raise TypeError("list contains non integer values")
         else:
-            counter_list = []
-            for i in range(len(numList)):
-                # loop based on the length of our data array
-                counter_list.append(numList.count(numList[i]))
-                # list of arrays counting frequency of values
-            maxFreq = max(counter_list)
-            if maxFreq == 1:
-                # if there are no modes, and the counter counts the mode once, its a failed test
+            i += 1
+    try:
+        for i in range(len(numList)):
+            if len(numList) <= 1:
                 return "no Mode"
-            # found maximum frequency of value, so value appears in list a certain number times
             else:
-                maxList = []
-                for x in numList:
-                    if (numList.count(x) == maxFreq):
-                        maxList.append(x)
-                        # extract that value
-                mode = []
-                for x in maxList:
-                    if (x not in mode):
-                    # remove duplicates
-                        mode.append(x)
-                return mode
+                counter_list = []
+                for i in range(len(numList)):
+                    # loop based on the length of our data array
+                    counter_list.append(numList.count(numList[i]))
+                    # list of arrays counting frequency of values
+                    maxFreq = max(counter_list)
+                    if maxFreq <= 1:
+                        # if there are no modes, and the counter counts the mode once, its a failed test
+                        return "no Mode"
+                    # found maximum frequency of value, so value appears in list a certain number times
+                    else:
+                        maxList = []
+                        for x in numList:
+                            if (numList.count(x) == maxFreq):
+                                maxList.append(x)
+                            # extract that value
+                        mode = []
+                        for x in maxList:
+                            if (x not in mode):
+                            # remove duplicates
+                                mode.append(x)
+                    return mode
     except TypeError:
         raise TypeError("list contains non integer values")
 
 
+
 def func_range(numList):
     try:
-        if numList == 0:
-            raise ValueError("numList contains zero to one ")
-        the_range = max(numList) - min(numList)
-        # get the maximum minus the minimum
-        return the_range
+        if len(numList) == 0:
+            raise ValueError("max () arg is an empty sequence")
+        else:
+            numList.sort()
+            # sort it
+            the_range = numList[len(numList) - 1] - numList[0]
+            # get the maximum minus the minimum
+            return the_range
     except TypeError:
         raise TypeError("list contains non integer values")
 
 
 def variance(numList):
     if len(numList) <= 1:
-        raise ValueError("no variance if there is only one value")
-    try:
-        mean = sum(numList)/len(numList)
-        # get the mean to use later
-        dev = []
-        #need these for later
-        for i in range (len(numList)):
-            var = (numList[i] - mean) ** 2
-            dev.append(var)
-            # shuffle through values and find individual variation
-            # find total variation
-        avg_var = sum(dev)/len(dev)
-        # average of total variation
-        return (avg_var)
-    #give back final variance
+        raise ValueError("no variance if there are one or less values")
+    else:
+        try:
+            mean = sum(numList)/len(numList)
+            # get the mean to use later
+            dev = []
+            #need these for later
+            for i in range (len(numList)):
+                var = (numList[i] - mean) ** 2
+                dev.append(var)
+                # shuffle through values and find individual variation
+                # find total variation
+            avg_var = sum(dev)/len(dev)
+            # average of total variation
+            return (avg_var)
+        #give back final variance
 
-    except TypeError:
-        raise TypeError("list contains non integer values")
+        except TypeError:
+            raise TypeError("list contains non integer values")
 
 def stnd_dev(numList):
-    try:
-
-        # return square root of variance rounded by 3 digits
-        return round(math.sqrt(variance(numList)), 3)
-    except TypeError:
-        raise TypeError("list contains non integer values")
+        if len(numList) <= 1:
+            raise ValueError("list contains one or less elements")
+        try:
+            # return square root of variance rounded by 3 digits
+            return round(math.sqrt(variance(numList)), 3)
+        except TypeError:
+            raise TypeError("list contains non integer values")
--- test_statTools.py
+++ test_statTools.py
@@ -7,38 +7,44 @@ def test_mode_basic():
     assert(mode([4, 5, 10]) == "no Mode")
 
 def test_mode_emptyList():
-    assert(mode([]) == -1)
+    assert(mode([]) == None)
 
 def test_mode_nonValue():
-    assert(mode([1, 1, 2, 3, 4, 5, "b", 5, 5, 5]) == "list contains non integer values")
+    with pytest.raises(TypeError) as error:
+        mode([1, 1, 2, 3, 4, "b", 5, 5])
+    assert("list contains non integer values" == str(error.value))
 
 def test_mode_OneValue():
-    assert(mode([1]) == [1])
+    assert(mode([1]) == "no Mode")
 
 def test_mode_oneType():
     assert(mode([1,1,1,1]) == [1])
 
 def test_mode_finalTest():
-    assert(mode([5, 4, 3 ,5 ,6 ,7, 4, 3, 4, 5, 6, 4 ,43 ,23 ,4 ,45, 64, 3 ,4]) == [4])
+    assert(mode([ 4, 3 ,6 ,7, 4, 3, 4, 4 ,3 ,4]) == [4])
 
 # FUNCTION: RANGE
 def test_range_basic():
     assert(func_range([0, 3]) == 3)
 
 def test_range_oneValue():
-    assert(func_range([0]) == 0)
+    assert(func_range([5]) == 0)
 
 def test_range_empty():
-    assert(func_range([]) == -1)
+    with pytest.raises(ValueError) as error:
+        func_range([])
+    assert("max () arg is an empty sequence" == str(error.value))
 
 def test_range_oneType():
-    assert(func_range([1, 1, 1] == 0))
+    assert(func_range([1, 1, 1, 1] == 0))
 
 def test_range_basic2():
     assert(func_range([1, 3, 4, 5, 5, 91]) == 90)
 
 def test_range_nonValue():
-    assert(func_range([1, 1, "yeet", 53]) == "list contains non integer values")
+    with pytest.raises(TypeError) as error:
+        func_range([1, 1, "yeet", 53])
+    assert("list contains non integer values") == str(error.value)
 
 # FUNCTION: VARIANCE
 
@@ -49,16 +55,22 @@ def test_variance_basic2():
     assert(variance([1, 2, 4, 5]) == 2.5)
 
 def test_variance_nonValue():
-    assert(variance([2, 3, 5, 5, 3, 4, 5, 6, "a"] == ""))
+    with pytest.raises(TypeError) as error:
+        variance([2, 3, 5, 5, 3, 4, 5, 6, "a"])
+    assert("list contains non integer values") == str(error.value)
 
 def test_variance_empty():
-    assert(variance([]) == -1)
+    with pytest.raises(ValueError) as error:
+        variance([])
+    assert("no variance if there are one or less values") == str(error.value)
 
 def test_variance_oneType():
     assert variance([-8, -8, -8, -8] == 0)
 
 def test_variance_oneValue():
-    assert(variance([3]) == -1)
+    with pytest.raises(ValueError) as error:
+        variance([3])
+    assert("no variance if there are one or less values") == str(error.value)
 
 # FUNCTION: STANDARD DEVIATION
 
@@ -66,16 +78,22 @@ def test_stndDev_basic():
     assert(stnd_dev([1, 2, 3]) == 0.816)
 
 def test_stndDev_nonValue():
-    assert(stnd_dev([1, 2, 4, "Code"]) == "list contains non integer values")
+    with pytest.raises(TypeError) as error:
+        stnd_dev([1, 2, 4, "Code"])
+    assert("list contains non integer values") == str(error.value)
 
 def test_stndDev_basic3():
     assert(stnd_dev([2, 3, 5, 5, 3, 4, 5, 6, 3]) == 1.247)
 
 def test_stndDev_empty():
-    assert(stnd_dev([]) == -1)
+    with pytest.raises(ValueError) as error:
+        stnd_dev([])
+    assert("list contains one or less elements") == str(error.value)
 
 def test_stndDev_oneType():
     assert(stnd_dev([-8, -8, -8, -8]) == 0)
 
 def test_stndDev_oneValue():
-    assert(stnd_dev([3]) == -1)
+    with pytest.raises(ValueError) as error:
+        stnd_dev([3])
+    assert("list contains one or less elements" == str(error.value))
